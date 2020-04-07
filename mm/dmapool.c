dcc46b18a44c9fc2f6766cb86e62bade0640fdca
Mateusz Nosek <mateusznosek0@gmail.com>
2020-04-06 20:08:49 -0700

    mm/dmapool.c: micro-optimisation remove unnecessary branch
    Previously there was a check if 'size' is aligned to 'align' and if not
    then it was aligned.  This check was expensive as both branch and division
    are expensive instructions in most architectures.  'ALIGN' function on
    already aligned value will not change it, and as it is cheaper than branch
    + division it can be executed all the time and branch can be removed.
    
    Signed-off-by: Mateusz Nosek <mateusznosek0@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20200320173317.26408-1-mateusznosek0@gmail.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/dmapool.c
+++ mm/dmapool.c
@@ -239,8 +239,8 @@ struct dma_pool *dma_pool_create(const char *name, struct device *dev,
 
 	if (size == 0 || size > INT_MAX)
 		return NULL;
-	if (size < sizeof(struct dma_block))
-		size = sizeof(struct dma_block);
+	else if (size < 4)
+		size = 4;
 
 	size = ALIGN(size, align);
 	allocation = max_t(size_t, size, PAGE_SIZE);

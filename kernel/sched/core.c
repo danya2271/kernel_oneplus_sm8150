3c92dca692724c6544bdc36b274754d4518331e5
EmanuelCN <emanuelghub@gmail.com>
2023-09-05 16:31:37 +0000

    kernel/sched: Cover more walt cases

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4088,7 +4088,9 @@ void scheduler_tick(void)
 	u64 wallclock;
 	bool early_notif;
 	u32 old_load;
+#ifdef CONFIG_SCHED_WALT
 	struct related_thread_group *grp;
+#endif
 	unsigned int flag = 0;
 	unsigned long thermal_pressure;
 
@@ -4123,6 +4125,7 @@ void scheduler_tick(void)
 #endif
 	rq_last_tick_reset(rq);
 
+#ifdef CONFIG_SCHED_WALT
 	rcu_read_lock();
 	grp = task_related_thread_group(curr);
 	if (update_preferred_cluster(grp, curr, old_load))
@@ -4134,6 +4137,7 @@ void scheduler_tick(void)
 
 	if (idle_cpu(cpu) && is_reserved(cpu))
 		clear_reserved(cpu);
+#endif
 }
 
 #ifdef CONFIG_NO_HZ_FULL
